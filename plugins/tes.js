148c90de1f668ea98db246a049978808dd685bd8
bobizabot <125097128+bobizabot@users.noreply.github.com>
2023-08-05 22:48:29 +0100

    Update tes.js

--- plugins/tes.js
+++ plugins/tes.js
@@ -1,9 +1,21 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `بوبيزة متصلة الان بالانترنيت \nيمكنك كتابة \n.menu\n لتنبتق لك جميع الاوامر \n@${m.sender.split('@')[0]} \nhttps://chat.whatsapp.com/FCudAHwTYLtJu242Zv1MmR`
+let info = `بوبيزة متصلة الان بالانترنيت \nيمكنك كتابة \n.menu\n لتنبتق لك جميع الاوامر \n@${m.sender.split('@')[0]} \n`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,
       isForwarded: true, externalAdReply: { title: author, body: bottime,thumbnail: fs.readFileSync('./thumbnail.jpg') }}})
+      await conn.sendMessage(m.chat, {
+        audio: {
+            url: "toma.mp3"
+        },
+        seconds: 256,
+        ptt: true,
+        mimetype: "audio/mpeg",
+        fileName: "vn.mp3",
+        waveform: [100, 0, 100, 0, 100, 0, 100]
+    }, {
+        quoted: m
+    })
 }
 handler.customPrefix = /^(tes|سلام|menu|Menu|apk|salam|hy|Hello|.|شكرا|مرحبا)$/i
 handler.command = new RegExp
